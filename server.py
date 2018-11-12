5d4073eb551880f22e5ff8866178d8a7740cb862
Tilia <lindsey.snider@gmail.com>
2018-11-11 17:13:05 -0800

    turning off debug mode

--- server.py
+++ server.py
@@ -813,7 +813,8 @@ def not_found(error):
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    app.debug = True
+    # app.debug = True
+    app.run()
     app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
     # make sure templates, etc. are not cached in debug mode
     app.jinja_env.auto_reload = app.debug
